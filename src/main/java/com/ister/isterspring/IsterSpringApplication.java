2aa84ef6422f5226674883e74f9342907a0f9157
hva23 <haskihossein@gmail.com>
2023-10-11 12:08:16 +0330

    init project

---
+++ src/main/java/com/ister/isterspring/IsterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ister.isterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsterSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ister/isterspring/IsterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ister.isterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
